7f1dcb47e420f02e0ff5e73deeb4849f56d515a2
= <brennocesar@live.com>
2020-09-10 11:09:41 -0400

    Completed Project

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,21 +1,73 @@
-import React, { useState } from 'react'
-import './NewTodoForm.css'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import {addTodoRequest} from '../Redux/thunks'
+import { getTodos } from '../Redux/selectors'
+import styled from 'styled-components'
 
-const NewTodoForm = () => {
+const FormContainer = styled.div`
+    border-radius: 8px;
+    padding: 16px;
+    text-align: center;
+    box-shadow: 0 4px 8px grey;
+`;
+const NewTodoInput = styled.input`
+    font-size: 16px;
+    padding: 8px;
+    border: none;
+    border-bottom: 2px solid #ddd;
+    border-radius: 8px;
+    width: 70%;
+    outline: none;
+`;
+const NewTodoButton = styled.button`
+    font-size: 16px;
+    padding: 8px;
+    border: none;
+    border-radius: 8px;
+    outline: none;
+    cursor: pointer;
+    margin-left: 8px;
+    width: 20%;
+    background-color: #22ee22;
+`;
+
+const NewTodoForm = ({ todos, onCreatePressed }) => {
     const [inputValue, setInputValue] = useState('');
     
     return (
-        <div className="new-todo-form">
-            <input className="new-todo-input" 
+        <FormContainer>
+            <NewTodoInput  
                 type="text" 
                 placeholder="Type your new todo here"
                 value={inputValue}
                 onChange={e => setInputValue(e.target.value)}
             />
-            <button className="new-todo-button">Create To Do</button>
-        </div>
+            <NewTodoButton 
+                onClick={() => {
+                    const isDuplicateText = todos.some(todo => todo.text === inputValue);
+                    if(!inputValue){
+                        alert('Empty Todo!')
+                    }
+                    else if(!isDuplicateText) {
+                        onCreatePressed(inputValue);
+                    }
+                    else{
+                        alert('Duplicated Todo!');
+                    }
+                    setInputValue('');
+                }}
+            >
+                Create Todo
+            </NewTodoButton>
+        </FormContainer>
     );
 }
 
+const mapStateToProps = state => ({
+    todos: getTodos(state),
+});
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(addTodoRequest(text)),
+});
 
-export default NewTodoForm;
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux'
+import styled from 'styled-components'
 import NewTodoForm from './NewTodoForm'
 import TodoListItem from './TodoListItem'
-import './TodoList.css'
+import {getTodosLoading, getIncompleteTodos, getCompletedTodos} from '../Redux/selectors'
+import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from '../Redux/thunks'
+import { isLoading } from '../Redux/reducers';
 
-const TodoList = ({todos = [{text: 'hello'}]}) => (
-    <div className="list-wrapper">
-        <NewTodoForm/>
-        {
-            todos.map(todo => <TodoListItem todo={todo}/> )
-        }
-    </div>
-)
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
+`;
 
-export default TodoList;
+const TodoList = ({getIncompleteTodos, getCompletedTodos, onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos}) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, [])
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <ListWrapper>
+            <NewTodoForm/>
+            {Object.keys(getIncompleteTodos).length > 0 ? <h3>Incomplete Todos</h3> : null}
+            {
+                getIncompleteTodos.map(todo => 
+                        <TodoListItem
+                        todo={todo}
+                        onRemovePressed={onRemovePressed}
+                        onCompletedPressed={onCompletedPressed}
+                        />
+                )
+            }
+            {Object.keys(getCompletedTodos).length > 0 ? <h3>Completed Todos</h3> : null}
+            {
+                getCompletedTodos.map(todo => 
+                        <TodoListItem
+                        todo={todo}
+                        onRemovePressed={onRemovePressed}
+                        onCompletedPressed={onCompletedPressed}
+                        />
+                )
+            } 
+        </ListWrapper>
+    );
+    return isLoading ? loadingMessage : content;
+    
+}
+
+const mapStateToProps = state => ({
+    isLoading: getTodosLoading(state),
+    getCompletedTodos: getCompletedTodos(state),
+    getIncompleteTodos: getIncompleteTodos(state),
+});
+const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: id => dispatch(removeTodoRequest(id)),
+    onCompletedPressed: id => dispatch(markTodoAsCompletedRequest(id)),
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
